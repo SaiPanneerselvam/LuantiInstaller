506f0d86d8bdd1b11032620f6e77c104b443317f
Sai Panneerselvam <sai.paarthiv@outlook.com>
2024-11-25 13:56:43 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.title = New System.Windows.Forms.Label()
+        Me.description = New System.Windows.Forms.Label()
+        Me.option1 = New System.Windows.Forms.Button()
+        Me.closebutton = New System.Windows.Forms.Button()
+        Me.option2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'title
+        '
+        Me.title.AutoSize = True
+        Me.title.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.title.Location = New System.Drawing.Point(17, 16)
+        Me.title.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.title.Name = "title"
+        Me.title.Size = New System.Drawing.Size(207, 31)
+        Me.title.TabIndex = 0
+        Me.title.Text = "Luanti Installer"
+        '
+        'description
+        '
+        Me.description.AutoSize = True
+        Me.description.Location = New System.Drawing.Point(24, 52)
+        Me.description.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.description.Name = "description"
+        Me.description.Size = New System.Drawing.Size(150, 16)
+        Me.description.TabIndex = 1
+        Me.description.Text = "Install Luanti on your PC."
+        '
+        'option1
+        '
+        Me.option1.Location = New System.Drawing.Point(491, 315)
+        Me.option1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.option1.Name = "option1"
+        Me.option1.Size = New System.Drawing.Size(100, 28)
+        Me.option1.TabIndex = 2
+        Me.option1.Text = "Start"
+        Me.option1.UseVisualStyleBackColor = True
+        '
+        'closebutton
+        '
+        Me.closebutton.BackColor = System.Drawing.Color.Red
+        Me.closebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.closebutton.ForeColor = System.Drawing.Color.White
+        Me.closebutton.Location = New System.Drawing.Point(531, -1)
+        Me.closebutton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.closebutton.Name = "closebutton"
+        Me.closebutton.Size = New System.Drawing.Size(100, 28)
+        Me.closebutton.TabIndex = 3
+        Me.closebutton.Text = "X - Close"
+        Me.closebutton.UseVisualStyleBackColor = False
+        '
+        'option2
+        '
+        Me.option2.Location = New System.Drawing.Point(368, 315)
+        Me.option2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.option2.Name = "option2"
+        Me.option2.Size = New System.Drawing.Size(100, 28)
+        Me.option2.TabIndex = 4
+        Me.option2.UseVisualStyleBackColor = True
+        Me.option2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 348)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.option2)
+        Me.Controls.Add(Me.closebutton)
+        Me.Controls.Add(Me.option1)
+        Me.Controls.Add(Me.description)
+        Me.Controls.Add(Me.title)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximumSize = New System.Drawing.Size(645, 395)
+        Me.MinimumSize = New System.Drawing.Size(645, 395)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Luanti Installer 5.10.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents title As System.Windows.Forms.Label
+    Friend WithEvents description As System.Windows.Forms.Label
+    Friend WithEvents option1 As System.Windows.Forms.Button
+    Friend WithEvents closebutton As System.Windows.Forms.Button
+    Friend WithEvents option2 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Net
+Imports System.IO
+Imports IWshRuntimeLibrary ' For creating shortcuts
+Imports Shell32 ' For extracting ZIP files
+
+Public Class Form1
+
+    Private Sub option1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles option1.Click
+        If option1.Text = "Start" Then
+            title.Text = "Ready to install?"
+            description.Text = "The Luanti (formerly Minetest) installer is an installer that downloads the latest copy of the" + Environment.NewLine + " luanti files from the official github download site and allows you to access the " + Environment.NewLine + " app via the Start Menu (for the installed user) instead of summoning the file from its location " + Environment.NewLine + "in the folder."
+            option1.Text = "Begin"
+            option2.Show()
+            option2.Text = "Back"
+        ElseIf option1.Text = "Begin" Then
+            title.Text = "Installing Luanti..."
+            description.Text = "Although it may seem not much is happening, something is."
+            option1.BackColor = Color.Gray
+            option1.Enabled = False
+            option2.BackColor = Color.Gray
+            option2.Enabled = False
+            closebutton.BackColor = Color.Gray
+            closebutton.Enabled = False
+            ' enter operations here later :)
+
+            Try
+                ' 1. Define paths
+                Dim username As String = Environment.UserName
+                Dim downloadUrl As New String("https://github.com/SaiPanneerselvam/SaiPanneerselvam/raw/refs/heads/main/quickfile/luanti-5.10.0-win64.zip") ' Replace with your actual URL
+                Dim zipFilePath As String = Path.Combine(Path.GetTempPath(), "temp.zip")
+
+                ' Concatenate paths using string concatenation
+                Dim extractPath As String = "C:\Users\" & username & "\Luanti"
+                Dim shortcutPath As String = "C:\Users\" & username & "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Luanti 5.10.0.lnk"
+                Dim minetestExePath As String = extractPath & "\bin\luanti.exe"
+
+                ' 2. Download the ZIP file
+                DownloadFile(downloadUrl, zipFilePath)
+
+                ' 3. Extract the ZIP file to the target directory
+                ExtractZipFile(zipFilePath, extractPath)
+
+                ' 4. Create a shortcut for minetest.exe
+                CreateShortcut(minetestExePath, shortcutPath)
+
+                ' Optionally, inform the user that the process was successful
+                MessageBox.Show("Minetest has been downloaded, extracted, and shortcut created.")
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            End Try
+            description.Text = "Installation Complete."
+            option1.Enabled = True
+            option2.Enabled = True
+            option2.BackColor = Color.FromArgb(240, 240, 240)
+            option1.BackColor = Color.FromArgb(240, 240, 240)
+            closebutton.BackColor = Color.Red
+            closebutton.Enabled = True
+        End If
+    End Sub
+
+    ' Function to download a file
+    Private Sub DownloadFile(ByVal url As String, ByVal destination As String)
+        ' Ensure TLS 1.2 is used for modern secure connections
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 Or SecurityProtocolType.Ssl3
+
+
+        Using client As New WebClient()
+            client.DownloadFile(url, destination)
+        End Using
+    End Sub
+
+
+
+    ' Function to extract a ZIP file using Shell32
+    Public Sub ExtractZipFile(ByVal zipFilePath As String, ByVal extractTo As String)
+        Dim shellApp As New Shell()
+
+        ' Check if the destination directory exists, if not, create it
+        If Not Directory.Exists(extractTo) Then
+            Directory.CreateDirectory(extractTo)
+        End If
+
+        ' Use Shell32 to extract the ZIP file
+        shellApp.NameSpace(extractTo).CopyHere(shellApp.NameSpace(zipFilePath).Items, 4)
+    End Sub
+
+    ' Function to create a shortcut
+    Private Sub CreateShortcut(ByVal targetPath As String, ByVal shortcutPath As String)
+        If System.IO.File.Exists(targetPath) Then
+            ' Create a WshShell object to interact with Windows Script Host
+            Dim shell As New WshShell()
+            ' Create a shortcut object at the specified path
+            Dim shortcut As IWshShortcut = CType(shell.CreateShortcut(shortcutPath), IWshShortcut)
+
+            ' Set the properties of the shortcut
+            shortcut.TargetPath = targetPath
+            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath)
+            shortcut.Description = "Luanti"
+            shortcut.IconLocation = targetPath ' You can change the icon if needed
+            shortcut.Save() ' Save the shortcut
+        Else
+            Throw New FileNotFoundException("Error, File is missing!")
+        End If
+    End Sub
+
+    ' Event handler for closing the form
+    Private Sub closebutton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles closebutton.Click
+        Close()
+    End Sub
+
+    ' Event handler for option2 button click
+    Private Sub option2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles option2.Click
+        If option2.Text = "Back" Then
+            title.Text = "Luanti Installer"
+            description.Text = "Install Luanti on your PC."
+            option1.Text = "Start"
+            option2.Hide()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
